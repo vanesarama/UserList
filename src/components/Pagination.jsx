1c6c50baa03b9dd7101499082436c94e3a1b2052
frog <frog@Tomass-MacBook-Air.local>
2024-02-18 17:00:16 +0100

    moved pagination code to Pagination component

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect } from "react";
+
+function Pagination({
+  prePage,
+  nextPage,
+  currentPage,
+  numbers,
+  changeCurrentPage,
+}) {
+  return (
+    <nav aria-label="Page navigation">
+      <ul className="flex pb-6 items-center -space-x-px h-8 text-sm">
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            onClick={prePage}
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              className="w-2.5 h-2.5 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+          </a>
+        </li>
+        {numbers.map((n, i) => (
+          <li key={i}>
+            <a
+              href="#"
+              className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${
+                currentPage === n ? "bg-blue-50 text-blue-600" : ""
+              } dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+              onClick={() => changeCurrentPage(n)}
+            >
+              {n}
+            </a>
+          </li>
+        ))}
+
+        <li>
+          <a
+            href="#"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            onClick={nextPage}
+          >
+            <span className="sr-only">Next</span>
+            <svg
+              className="w-2.5 h-2.5 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Pagination;
